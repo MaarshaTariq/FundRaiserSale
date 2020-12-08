75be34540085bc3c8af3b8adde226e24aa6c3699
Hamza.jahangir <hamza.jahangir@geniteam.com>
2020-12-08 11:59:14 +0500

    Merge Test_1
    - Some minor changes

--- FundraiserSale/Assets/Scripts/HighlightText.cs
+++ FundraiserSale/Assets/Scripts/HighlightText.cs
@@ -7,21 +7,25 @@ public class HighlightText : MonoBehaviour {
  public Image highlight_Image;
  public float maxFillAmount =350f;
  public static float fillAmountForImage;
+
  public void Start()
  {
- highlight_Image= GetComponent<Image>();	 
- fillAmountForImage= 1;
+     highlight_Image= GetComponent<Image>();	 
+     fillAmountForImage= 1;
  
  }
- public void Update()
- {
+    public void Update()
+     {
 	 
-	 if(highlight_Image.fillAmount < maxFillAmount){
-	 highlight_Image.fillAmount = fillAmountForImage * Time.time *0.2f;
-	 }
- }
- public void IncreaseFillAmount(){
+	    if(highlight_Image.fillAmount < maxFillAmount)
+        {
+	        highlight_Image.fillAmount = fillAmountForImage * Time.time *0.2f;
+	    }
+    }
+
+    public void IncreaseFillAmount()
+    {
 	 fillAmountForImage +=1f  ;
- }
+     }
 
 }
